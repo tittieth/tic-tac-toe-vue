9afd0f38b80e32223203c66f096c8b92d2467b93
tittieth <114925706+tittieth@users.noreply.github.com>
2023-06-01 10:03:57 +0200

    add styling for form

--- src/components/GetPlayers.vue
+++ src/components/GetPlayers.vue
@@ -26,19 +26,49 @@ const savePlayersToLs = () => {
 </script>
 
 <template>
- <div>
-    <div>
-      <form @submit.prevent="savePlayersToLs">
-        <h3>Player X: </h3>
-        <input type="text" placeholder="Enter your name" v-model="players.playerX.name"/>
-        <h3>Player O: </h3>
-        <input type="text" placeholder="Enter your name" v-model="players.playerO.name"/>
-        <button>Start Game</button>
-      </form>
-    </div>
+ <div class="wrapper"> 
+    <form @submit.prevent="savePlayersToLs">
+      <h3>Player X: </h3>
+      <input type="text" placeholder="Enter your name" v-model="players.playerX.name"/>
+      <h3>Player O: </h3>
+      <input type="text" placeholder="Enter your name" v-model="players.playerO.name"/>
+      <button>Start Game</button>
+    </form>
   </div>
 </template>
 
 <style scoped>
+
+.wrapper {
+  height: 100vh;
+  width: 100vw;
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+}
+
+h3 {
+  padding: 10px;
+  margin: 10px;
+  font-size: 2em;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+}
+
+input {
+  padding: 20px;
+  border-radius: 8px;
+  border: 1px solid transparent;
+}
+
+button {
+  margin: 50px;
+  padding: 20px 45px;
+}
 
 </style>
